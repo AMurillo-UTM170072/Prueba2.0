fde36229e6c7b6be932bee30b341023a8623db0c
AMurillo-UTM170072 <68796376+AMurillo-UTM170072@users.noreply.github.com>
2021-04-26 11:14:25 -0500

    Delete src/configuration directory

--- src/configuration/constans.ts
+++
@@ -1,7 +0,0 @@
-// estas constantes son mandadas llamar del env cambia el nombre del usuario y la contraseña
-export const SERVER_PORT='SERVER_PORT';
-export const DB_PORT='DB_PORT'
-export const DB_NAME='DB_NAME'
-export const DB_HOST='DB_HOST'
-export const DB_USER='DB_USER'
-export const DB_PASSWORD='DB_PASSWORD'
