00d7b51c4f14fc98a5269b0665b93834aa5ab77d
AMurillo-UTM170072 <68796376+AMurillo-UTM170072@users.noreply.github.com>
2021-04-26 11:13:20 -0500

    Delete src/usuarios directory

--- src/usuarios/usuarios.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { UsuarioEntity } from './usuarios.entity';
-
-@EntityRepository(UsuarioEntity)
-export class UsuarioRepository extends Repository<UsuarioEntity> {}
